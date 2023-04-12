2318c153cd1f7a23dd5d003dd98b899f6f03d3d1
Urukagall <dany.jorge@free.fr>
2023-04-12 13:00:20 +0200

    init 2

---
+++ main.py
@@ -0,0 +1,31 @@
+# Init
+print("Hello World")
+import pygame
+import pygame.time
+pygame.init()
+clock = pygame.time.Clock()
+
+# Create Window
+displayHeight = 1080
+displayWidth = 1920
+backgroundColor = (200,200,200)
+screen = pygame.display.set_mode((displayWidth, displayHeight), pygame.FULLSCREEN)
+
+
+# Main Loop
+running = True
+while (running):
+    # run the game at a constant 60fps
+    clock.tick(60)
+    #Did the user clicked the close button ?
+    for events in pygame.event.get():
+        if events.type == pygame.QUIT:
+            running=False
+        elif events.type == pygame.KEYDOWN:
+            if events.key == pygame.K_ESCAPE:
+                running=False
+
+    #Draw 
+    screen.fill(backgroundColor)
+    pygame.display.update()
+pygame.quit()
