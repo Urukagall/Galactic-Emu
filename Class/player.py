59fbe1a944af224ea1c079bda17716d666d97651
Coucoul38 <noham.coulon@gmail.com>
2023-04-14 13:46:03 +0200

    vector normalisation

---
+++ Class/player.py
@@ -0,0 +1,39 @@
+import math
+import pygame
+class Player:
+    def __init__(self, basicSpeed, slowSpeed, size, img, displayWidth, displayHeight, dashSpeed,cooldownDash,timeDash, health):
+        self.X = 100
+        self.Y = 100
+        self.basicSpeed = basicSpeed
+        self.slowSpeed = slowSpeed
+        self.speed = basicSpeed
+        self.size = size
+        self.img = img
+        self.displayWidth = displayWidth
+        self.displayHeight = displayHeight
+        self.dashSpeed = dashSpeed
+        self.cooldownDash = cooldownDash
+        self.timeDash = timeDash
+        self.health = health
+        self.img = pygame.transform.scale(self.img, (self.size, self.size))
+
+    def move(self, veloX, veloY):
+        if veloX != 0 and veloY != 0:
+            self.X = self.X + math.sqrt(1/2) * self.speed * veloX
+            self.Y = self.Y + math.sqrt(1/2) * self.speed * veloY
+        else:
+            self.X = self.X + veloX * self.speed
+            self.Y = self.Y + veloY * self.speed
+        
+        #Stop player from going out of the screen
+        if self.X > self.displayWidth - self.size:
+            self.X = self.displayWidth - self.size
+        if self.X < 0:
+            self.X = 0
+        if self.Y > self.displayHeight - self.size:
+            self.Y = self.displayHeight - self.size
+        if self.Y < 0:
+            self.Y = 0
+    
+    def takeDmg(self, dmg):
+        self.health -= dmg
