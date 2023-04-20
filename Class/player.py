ce6c0d18f782b2e0ae9ed328e80c546aa8ef5d65
Urukagall <dany.jorge@free.fr>
2023-04-20 16:14:29 +0200

    s

--- Class/player.py
+++ Class/player.py
@@ -5,8 +5,8 @@ from Class.particle import Particle
 
 class Player():
     def __init__(self, basicSpeed, slowSpeed, size, displayWidth, displayHeight, dashSpeed,cooldownDash,timeDash, lives, projectileList, imgBullet, imgMissile):
-        self.X = 0
-        self.Y = 0
+        self.X = 800
+        self.Y = 500
         self.basicSpeed = basicSpeed
         self.slowSpeed = slowSpeed
         self.speed = basicSpeed
